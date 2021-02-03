f9488fe00e529209d0726529eeaf58dc57bb0bfc
Cozy <gaunletgames@gmail.com>
2021-02-02 22:37:52 -0500

    Git Ignore Update

--- Decoupler/Assets/cozy_packages/actors/BoxActor.cs
+++ Decoupler/Assets/cozy_packages/actors/BoxActor.cs
@@ -3,11 +3,12 @@ using UnityEngine;
 
 namespace com.cozyhome.Actors
 {
-    [RequireComponent(typeof(BoxCollider))] public class BoxActor : ActorHeader.Actor
+    [RequireComponent(typeof(BoxCollider))]
+    public class BoxActor : ActorHeader.Actor
     {
         [System.NonSerialized] private ArchetypeHeader.BoxArchetype BoxArchetype;
         /* UnityEngine */
-        void Start() 
+        void Start()
         {
             SetPosition(transform.position);
             SetOrientation(transform.rotation);
@@ -20,7 +21,7 @@ namespace com.cozyhome.Actors
 
         public override ArchetypeHeader.Archetype GetArchetype() => BoxArchetype;
 
-        public override bool DetermineGroundStability(Vector3 _vel, RaycastHit _hit, LayerMask _gfilter) 
+        public override bool DetermineGroundStability(Vector3 _vel, RaycastHit _hit, LayerMask _gfilter)
             => base.DeterminePlaneStability(_hit.normal, _hit.collider);
     }
 }
